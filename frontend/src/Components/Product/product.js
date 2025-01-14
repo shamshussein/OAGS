88aca43941b44dd4d723dc991cb76289449072c9
shamshussein <hussein_shams@outlook.com>
2025-01-15 01:29:04 +0200

    changes

--- frontend/src/Components/Product/product.js
+++ frontend/src/Components/Product/product.js
@@ -3,18 +3,18 @@ import Sidebar from './Sidebar';
 import ProductList from './ProductList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const products = [
-  {
-    id: 1,
-    name: 'Tent',
-    price: 100,
-    category: 'Camping',
-    rating: 4.5,
-    image: '/assets/images/camping/Camping chair.jpg',
-    description: 'A great camping tent.',
-    isSized: true,
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: 'Tent',
+//     price: 100,
+//     category: 'Camping',
+//     rating: 4.5,
+//     image: '/assets/images/camping/Camping chair.jpg',
+//     description: 'A great camping tent.',
+//     isSized: true,
+//   },
+// ];
 
 function Product() {
   const [filteredProducts, setFilteredProducts] = useState(products);
