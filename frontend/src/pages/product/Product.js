283e5d53ba4671f594402c785be3584d268aba0b
shamshussein <hussein_shams@outlook.com>
2025-02-11 01:30:33 +0200

    upgrade

--- frontend/src/pages/product/Product.js
+++ frontend/src/pages/product/Product.js
@@ -34,7 +34,6 @@ function Product() {
             priceRange: selectedPriceRange 
           },
         });
-
         setProductsByCategory((prev) => ({
           ...prev,
           [category]: append
